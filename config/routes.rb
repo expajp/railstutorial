31db7070ed5f370077de1623b97f5ba8f88c4986
Shu OGAWARA <expajp@gmail.com>
2017-10-26 11:17:46 +0900

    add link to signup page in home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
 
@@ -10,6 +9,9 @@ Rails.application.routes.draw do
   get '/about', to:'static_pages#about'
 
   get '/contact', to:'static_pages#contact'
+
+  get '/signup', to:'users#new'
+    
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
