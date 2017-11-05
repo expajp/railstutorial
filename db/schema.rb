56548206fb8e865dad0382b8126b829d676b3e14
Shu OGAWARA <expajp@gmail.com>
2017-11-05 22:24:46 +0900

    add migration to add password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103033308) do
+ActiveRecord::Schema.define(version: 20171105132043) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_sigest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
