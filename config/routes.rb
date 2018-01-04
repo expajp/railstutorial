24b20f8c43d9833c8b7dc9bff08b94f22ef3ee61
Shu OGAWARA <expajp@gmail.com>
2018-01-04 23:35:55 +0900

    generate controller of account-activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
   get '/home', to:'static_pages#home'
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
     
   resources :users
+  resources :account_activations, only: [:edit]
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
