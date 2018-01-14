ef47d1a36f0afe3d78725b5810cd9a77d26059a0
Shu OGAWARA <expajp@gmail.com>
2018-01-15 00:03:43 +0900

    generate controller for password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
@@ -13,5 +14,6 @@ Rails.application.routes.draw do
     
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   
 end
